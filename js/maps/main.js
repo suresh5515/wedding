c8de173e6aea22270cfd20d4fc5026917e52ccf6
suresh <suresh.p@cybosol.com>
2017-05-08 12:01:56 +0530

    map aded

--- js/maps/main.js
+++ js/maps/main.js
@@ -1,19 +1,19 @@
  /* ======= Google Map ======= */
     map = new GMaps({
         div: '#map',
-        lat: 8.899519,//10.9636069, 
-        lng: 77.827547,//78.07571411,
+        lat: 8.899519, 
+        lng: 77.827547,
         scrollwheel: false,
-        zoom: 10,
+        zoom: 9,
     });
-
+    
     map.addMarker({
-        lat: 10.960673, 
-        lng: 78.078985,
+        lat: 8.948138, 
+        lng: 77.275071,
         verticalAlign: 'top',
         title: 'Wedding Location',  
         infoWindow: {
-            content: '<div class="note">Wedding</div><h4 class="map-title script">Arulmigu Kalyana Pasupatheswarar Temple</h4><div class="address"><span class="region">Karur</span></div>'
+            content: '<div class="note">Wedding</div><h4 class="map-title script">Thiru kumaran kovil</h4><div class="address"><span class="region">Ilanji</span><br/><span class="region">Tirunelveli</span></div>'
         }
         
         
@@ -21,28 +21,28 @@
     
 
     map.addMarker({
-        lat: 11.103623,
-        lng: 78.008568,
+        lat: 8.495749,
+        lng: 78.128578,
         title: 'Reception Location',      
         infoWindow: {
-            content: '<div class="note">Reception</div><h4 class="map-title script">Kokila Marriage Hall</h4><div class="address"><span class="region">Cavery River Rd, Paramathi Velur,</span><br><span class="postal-code">Namakkal</span></div>'
+            content: '<div class="note">Reception</div><h4 class="map-title script">Bhajana Mandapam</h4><div class="address"><span class="region">Thiruchendur</span><br><span class="postal-code">Tuticorin</span></div>'
         } 
         
     });
-	
-	map.addMarker({
-        lat: 10.961416,
-        lng: 78.086095,
+    
+    map.addMarker({
+         lat: 8.958522,
+        lng: 77.280005,
         title: 'Wedding Party',      
         infoWindow: {
-            content: '<div class="note">Wedding Party</div><h4 class="map-title script">Archana Hotel</h4><div class="address"><span class="region">Karur</span></div>'
+            content: '<div class="note">Wedding Party</div><h4 class="map-title script">Kuthalingam Bethammal Marriage Hall</h4><div class="address"><span class="region">Ilanji</span><br><span class="postal-code">Tirunelveli</span></div>'
         } 
         
     });
     
     /*display marker 1 address on load */
-    google.maps.event.trigger(map.markers[0], 'load');
+    google.maps.event.trigger(map.markers[0], 'click');
     /*display marker 2 address on load */
-    google.maps.event.trigger(map.markers[1], 'load');
-	
-	google.maps.event.trigger(map.markers[2], 'load');
+    google.maps.event.trigger(map.markers[1], 'click');
+    
+    google.maps.event.trigger(map.markers[2], 'click');
